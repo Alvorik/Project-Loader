c52d206f3d47f1b73b71ed342030457252d09ba6
Alvorik <115509470+Alvorik@users.noreply.github.com>
2023-02-06 18:08:18 -0600

    Update Plat Hub V2.1 Alpha.lua

--- Plat Hub V2.1 Alpha.lua
+++ Plat Hub V2.1 Alpha.lua
@@ -1,15 +1,111 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Project X Loader", "GrapeTheme")
+-- Welcome to Project Valix, If you're seeing this apply for developer in Our Discord ("https://discord.gg/t7sXgFNmgq")
+-- This Script Hub was Created by ALVORIKK and is Currently being worked on By the Developers in the discord above.
 
--- Main Section
-local Main = Window:NewTab("Project Valix")
-local MainSection = Main:NewSection("Project Valix Loader")
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Project Valix Loader",
+    LoadingTitle = "Project Valix Development",
+    LoadingSubtitle = "by Developers",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil, -- Create a custom folder for your hub/game
+       FileName = "Valix"
+    },
+    Discord = {
+       Enabled = false,
+       Invite = "t7sXgFNmgq", -- The Discord invite code, do not include discord.gg/
+       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
+    KeySystem = false, -- Set this to true to use our key system
+    KeySettings = {
+       Title = "Project Valix",
+       Subtitle = "Key System",
+       Note = "Join the discord (discord.gg/t7sXgFNmgq)",
+       FileName = "ValixSKey",
+       SaveKey = false,
+       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       Key = "ValixIsHere"
+    }
+})
 
 
-MainSection:NewButton("Load Project Valix", "Loads Project Valix", function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Alvorik/Project-Valix/main/ProjectValix", true))()
-end)
+-- Menu Stuff
+local Loader = Window:CreateTab("Loader") -- Title, Image
+local MenuSection = Loader:CreateSection("Project Valix Loader Stuff")
 
-MainSection:NewButton("Load Old Project X", "Loads The Old Project X Might get you banned", function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Alvorik/Plat-Hub-V2/main/Plat%20Hub%20V2%20Alpha.lua", true))()
-end)
+getgenv().SelectedStuff = nil
+
+getgenv().SelectedOption = true
+
+-- Functions
+
+
+
+function SelectedScriptHub()
+    if getgenv().SelectedOption == true then do
+        getgenv().SelectedStuff
+    end
+
+end
+-- Buttons
+
+local Button = Loader:CreateButton({
+    Name = "Execute New Project Valix",
+    Callback = function()
+        getgenv().SelectedOption = true
+        SelectedScriptHub()
+
+        Rayfield:Notify({
+            Title = "Project Valix | Loader",
+            Content = "Loading..",
+            Duration = 6.5,
+            Image = 0,
+            Actions = { -- Notification Buttons
+               Ignore = {
+                  Name = "Okay!",
+                  Callback = function()
+                  print("The user tapped Okay!")
+               end
+            },
+         },
+        })
+
+        wait(1)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Alvorik/Project-Valix/main/ProjectValix", true))()
+
+
+
+    end,
+})
+
+local Button = Loader:CreateButton({
+    Name = "Execute Old Valix",
+    Callback = function()
+        getgenv().SelectedOption = true
+        SelectedScriptHub()
+
+        Rayfield:Notify({
+            Title = "Project Valix | Loader",
+            Content = "Loading..",
+            Duration = 6.5,
+            Image = 0,
+            Actions = { -- Notification Buttons
+               Ignore = {
+                  Name = "Okay!",
+                  Callback = function()
+                  print("The user tapped Okay!")
+               end
+            },
+         },
+        })
+
+
+        wait(1)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Alvorik/Plat-Hub-V2/main/Plat%20Hub%20V2%20Alpha.lua", true))()
+
+
+    end,
+})
+
+
